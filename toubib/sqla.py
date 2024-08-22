eeabdae001c8e0a56f641228c41b828ad7491194
Shalom Steinmetz <shalomsteinmetz@gmail.com>
2024-08-22 16:13:32 -0400

    add Patient model

--- toubib/sqla.py
+++ toubib/sqla.py
@@ -1,5 +1,11 @@
 from fastapi_sqla import Base
+from sqlalchemy import Column, String
 
 
 class Doctor(Base):
     __tablename__ = "doctor"
+
+
+class Patient(Base):
+    __tablename__ = "patient"
+    last_name = Column(String)
