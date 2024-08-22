b7ed7d657b78fd8059ecf673d61b16737ab2ce51
Shalom Steinmetz <shalomsteinmetz@gmail.com>
2024-08-22 16:12:41 -0400

    add patient migration

--- toubib/db/versions/002_add_patient_table.py
+++ toubib/db/versions/002_add_patient_table.py
@@ -5,6 +5,7 @@ Revises:
 Create Date: 2021-06-02 14:31:33.813799
 
 """
+
 import sqlalchemy as sa
 from alembic import op
 
@@ -16,10 +17,28 @@ depends_on = None
 
 
 def upgrade():
-    # 🥸 This is where you add patient table.
-    pass
+    op.create_table(
+        "patient",
+        sa.Column(
+            "id", sa.Integer, sa.Identity(always=True), nullable=False, primary_key=True
+        ),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.func.current_timestamp(),
+            nullable=False,
+        ),
+        sa.Column("first_name", sa.String, nullable=False),
+        sa.Column("last_name", sa.String, nullable=False),
+        sa.Column("date_of_birth", sa.Date, nullable=False),
+        sa.Column(
+            "sex_at_birth",
+            sa.Enum("FEMALE", "MALE", name="sex_at_birth_enum"),
+            nullable=False,
+        ),
+        sa.Column("email", sa.String, nullable=False),
+    )
 
 
 def downgrade():
-    # 🥸 This is where you drop it.
-    pass
+    op.drop_table("patient")
