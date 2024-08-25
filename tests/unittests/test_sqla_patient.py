37a6f0e5866e3b4391f90419f73f51208f2828b0
Shalom Steinmetz <shalomsteinmetz@gmail.com>
2024-08-24 23:41:54 -0400

    test for no duplicate patient email

--- tests/unittests/test_sqla_patient.py
+++ tests/unittests/test_sqla_patient.py
@@ -1,6 +1,8 @@
 def test_add_patient(session, faker):
     from toubib.sqla import Patient
     from datetime import datetime
+    from pytest import raises
+    from sqlalchemy.exc import IntegrityError
 
     patient = Patient(
         email=faker.ascii_email(),
@@ -12,3 +14,13 @@ def test_add_patient(session, faker):
     session.add(patient)
     session.flush()
     assert patient.id is not None
+    patient2 = Patient(
+        email=patient.email,
+        first_name=faker.first_name(),
+        last_name=faker.last_name(),
+        date_of_birth=datetime.strptime(faker.date(), "%Y-%m-%d"),
+        sex_at_birth=faker.random_element(["FEMALE", "MALE"]),
+    )
+    session.add(patient2)
+    with raises(IntegrityError):
+        session.flush()
